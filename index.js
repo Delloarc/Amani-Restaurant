a292077357b696f2ae161d6ee239e46c3f520766
Chris <87422346+Delloarc@users.noreply.github.com>
2022-12-29 18:19:30 +0100

    Add files via upload
    Google maps

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ function closeNav() {
   document.getElementById("navbar-links").style.display = "none";
   document.getElementById("toggle-button").style.display = "block";
 }
+
+var map = document.getElementById("mymap");
+var maplistner = function (e) {
+  var frame = document.createElement("iframe");
+  frame.src = this.getAttribute("data-src");
+  map.appendChild(frame);
+  map.removeEventListener("mouseover", maplistner);
+};
+map.addEventListener("mouseover", maplistner); 
