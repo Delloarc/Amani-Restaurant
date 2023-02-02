ee5d9e787dad9cd4d090f338702cacb095b77b92
Chris <87422346+Delloarc@users.noreply.github.com>
2023-02-02 17:11:56 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -25,31 +25,3 @@ function closeNav() {
 var x = window.matchMedia("(max-width: 580px)");
 
 
-// Image slider
-
-const swiper = new Swiper(".swiper", {
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
